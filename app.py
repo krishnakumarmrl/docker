0c0fbca4d82d3b544e7dc117347b4d402531f63f
Krishnakumar M <krishna@example.com>
2020-11-16 16:42:12 +0800

    3rd commit break

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def response():
+#@app.route('/')
+#def response():
   return "Hello world from a Flask container. Newly added"
 
 if __name__ == '__main__':
