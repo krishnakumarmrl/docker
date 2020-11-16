0734b112b54964c75caba28fdb9a89a240f6897e
Krishnakumar M <krishna@example.com>
2020-11-16 17:30:24 +0800

    2nd update in app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def response():
-  return "Hello world from a Flask container. Newly added"
+  return "Hello world from a Flask container. Newly added. 2nd added"
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=80)
